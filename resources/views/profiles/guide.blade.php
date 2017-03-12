474b597740ab191311a4c5cf489e03587f18a980
Gary Clarke <garyclarke1974@gmail.com>
2017-03-12 19:17:25 +0000

    Edit profile guide text

--- resources/views/profiles/guide.blade.php
+++ resources/views/profiles/guide.blade.php
@@ -6,7 +6,7 @@
 
             <h3>How Do We Achieve This?</h3>
 
-                <p>Our intelligent points system helps to identify the behaviour of touts and that of genuine fans. There is no single step which will identify an individual as a tout but with automated tracking of user behaviour we can build the picture.</p>
+                <p>Our intelligent points system helps to identify the behaviour of touts and that of genuine fans. There is no single step which will identify an individual as a tout but by tracking user behaviour we can build the picture.</p>
 
             <h3>How Are The Points Awarded?</h3>
             <p>In short, points are awarded for every action which would indicate that the customer is a genuine concert goer. A few examples:</p>
@@ -15,7 +15,7 @@
                 <li>Registering in advance of events</li>
                 <li>Payment details which cross reference accurately against the profile information.</li>
                 <li>Not always taking the full allocation of tickets</li>
-                <li>Having <i>sensible</i> wishlists which reflect those of genuine fans</li>
+                <li>Having <i>sensible</i> wishlists which reflect those of genuine fans.</li>
             </ul>
             <h3>Is The System Tout Proof?</h3>
             <p>Yes and no. We can't keep touts from using the site but they'll be wasting their time if they think that they are going to make money from it. They'd be better off waiting tables.</p>
