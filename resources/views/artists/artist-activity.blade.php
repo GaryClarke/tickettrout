712a3f54afeeb95cfb42eadcf20daf2dd744a554
Gary Clarke <garyclarke1974@gmail.com>
2017-03-13 18:50:38 +0000

    Revert artist video to original dynamic code

--- resources/views/artists/artist-activity.blade.php
+++ resources/views/artists/artist-activity.blade.php
@@ -7,10 +7,9 @@
             </p>
 
             <div class="media">
-                {{--<div style="position:relative;height:0;padding-bottom:56.25%">--}}
-                    {{--<iframe src="{{ $artist->video }}" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe>--}}
-                {{--</div>--}}
-                <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/k8TFiKDQC78?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+                <div style="position:relative;height:0;padding-bottom:56.25%">
+                    <iframe src="{{ $artist->video }}" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe>
+                </div>
             </div>
 
             <h3>
